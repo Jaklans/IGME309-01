4140a9195124d390288cb4205071c1261e655851
jxs2828 <jxs2828@IGM2000-09>
2018-02-03 22:39:50 -0500

    Add to repo

---
+++ E03 Complementary Colors/Shaders/BasicColor.fs
@@ -0,0 +1,21 @@
+#version 330
+
+in vec3 Color;
+uniform int flip;
+
+uniform vec3 SolidColor = vec3(-1,-1,-1);
+
+out vec4 Fragment;
+
+void main()
+{
+	if(flip == 0)
+		Fragment = vec4(Color ,1);
+	else
+		Fragment = vec4(1-Color, 1);
+	
+	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
+		Fragment = vec4(SolidColor, 1);
+
+	return;
+}
